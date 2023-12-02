9c73cdecb844aad2083444edd49d2409c7de6c37
ChinhVo853 <chinhvo853@gmail.com>
2023-12-02 20:32:17 +0700

    capnhatAPI

--- app/Http/Controllers/SanPhamAPIController.php
+++ app/Http/Controllers/SanPhamAPIController.php
@@ -8,7 +8,12 @@ use App\Models\SanPham;
 class SanPhamAPIController extends Controller
 {
     public function Danh_Sach_San_Pham(){
-        $sanPham = SanPham::all();
+        $sanPham = SanPham::orderBy('id', 'desc')->get();
+        
+        foreach($sanPham as $item)
+        {
+            $item->hinh_anh;
+        }
         return response()->json([
             'success' => true,
             'data' => $sanPham
