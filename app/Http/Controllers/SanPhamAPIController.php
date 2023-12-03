79a1690d5ad7f4604cf8234d9a85d0e00c7fa83e
ChinhVo853 <chinhvo853@gmail.com>
2023-12-03 21:54:48 +0700

    api

--- app/Http/Controllers/SanPhamAPIController.php
+++ app/Http/Controllers/SanPhamAPIController.php
@@ -7,13 +7,26 @@ use App\Models\SanPham;
 
 class SanPhamAPIController extends Controller
 {
-    public function Danh_Sach_San_Pham(){
+    public function DanhSachSanPham(){
         $sanPham = SanPham::orderBy('id', 'desc')->get();
         
         foreach($sanPham as $item)
         {
             $item->hinh_anh;
         }
+        return response()->json([
+            'success' => true,
+            'data' => $sanPham
+        ]);
+    }
+
+    public function ChiTietSanPham($id){
+        $sanPham = SanPham::where('id',$id)->first();
+        
+       
+        
+        $sanPham ->hinh_anh;
+        
         return response()->json([
             'success' => true,
             'data' => $sanPham
